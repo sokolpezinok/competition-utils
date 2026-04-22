be691bc0f0a99298e0af06b03fa6be34982104e2
Lukáš Poláček <lukas@polacek.email>
2026-04-22 19:25:31 +0200

    Add two initial scripts

---
+++ distribute_fees.py
@@ -0,0 +1,58 @@
+from xml.etree import ElementTree as ET
+import sys
+from collections import defaultdict
+
+assert len(sys.argv) >= 2
+
+ET.register_namespace("", "http://www.orienteering.org/datastandard/3.0")
+NS = {"iof": "http://www.orienteering.org/datastandard/3.0"}
+tree = ET.parse(sys.argv[1])
+ET.indent(tree)
+
+# TODO: parse some known format, e.g. export from IS SZOŠ
+payments = defaultdict(
+    float,
+    {
+        "TJ Rapid Bratislava": 183.0,
+        "HSP orienteering Láb": 16.0,
+        "Športový klub Sandberg": 302.0,
+        "Klub OB Sokol Pezinok": 225.0,
+        "KOBRA Bratislava": 21.0,
+        "ŠK Farmaceut Bratislava": 84.0,
+        "ŠK VAZKA Bratislava": 159.0,
+        "TJ Ioan 1209": 8.0,
+        "Neregistrovani/ No Club/ Vereinslos": 0.0,
+        "Neregistrovaný": 7.0,
+        "Slávia Žilinská univerzita": 8.0,
+        "Pukinov dvor": 8.0,
+        "Korytnačky": 16.0,
+        "Roxor": 8.0,
+        "Viatoris": 0.0,
+        "GAMČA": 5.0,
+        "(AUT) Naturfreunde Wien": 8.0,
+        "(AUT) HSV OL Wiener Neustadt": 29.0,
+        "(AUT) OJE Wappler": 8.0,
+        "ŠK HADVEO Banská Bystrica": 44.0,
+        "Slovenská triatlonová akadémia": 26.0,
+        "": 0.0,
+    },
+)
+
+for person_entry in tree.getroot().findall("iof:PersonEntry", NS):
+    club_name = person_entry.find("./iof:Organisation/iof:Name", NS).text
+    fee = person_entry.find("./iof:AssignedFee/iof:Fee/iof:Amount", NS).text
+    assert fee is not None
+    fee = float(fee)
+
+    club_total = payments.get(club_name, 0.0)
+    available = min(club_total, fee)
+    payments[club_name] -= available
+
+    fee_entry = person_entry.find("./iof:AssignedFee", NS)
+    assert fee_entry is not None
+    paid = ET.SubElement(fee_entry, "PaidAmount", {"currency": "EUR"})
+    paid.text = str(available)
+
+
+ET.indent(tree)
+ET.dump(tree)
---
+++ pomp_score.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# Ráta body za Pohár Oslobodenia Mesta Pezinok
+# 3, 2, 1 body sú za 1. až 3. miesto v každej kategórii
+# Dáta idú z IOF XML formátu výsledkov
+# $ ./pomp.py results.xml
+
+import xml.etree.ElementTree as ET
+import csv
+import sys
+
+assert len(sys.argv) >= 2
+
+tree = ET.parse(sys.argv[1])
+ET.indent(tree)
+NS = {"iof": "http://www.orienteering.org/datastandard/3.0"}
+
+POSITION_POINTS = {1: 3, 2: 2, 3: 1}
+
+club_points = {}
+classes = set()
+for class_result in tree.getroot().findall("iof:ClassResult", NS):
+    clas = class_result.find("./iof:Class/iof:Name", NS).text
+    classes.add(clas)
+    for person_result in class_result.findall("./iof:PersonResult", NS):
+        status = person_result.find("./iof:Result/iof:Status", NS)
+        if status is None or status.text != "OK":
+            continue
+        position = int(person_result.find("./iof:Result/iof:Position", NS).text)
+        if position >= 4:
+            continue
+        club = person_result.find("./iof:Organisation", NS)
+        if club is None:
+            continue
+        club = person_result.find("./iof:Organisation/iof:Name", NS)
+        assert club is not None
+        club = club.text
+        if club not in club_points:
+            club_points[club] = {}
+
+        points = POSITION_POINTS[position]
+        club_points[club][clas] = club_points[club].setdefault(clas, 0) + points
+
+csv_writer = csv.writer(sys.stdout)
+club_table = list(club_points.items())
+club_table.sort()
+
+classes = list(classes)
+classes.sort()
+
+clubs, point_maps = list(zip(*club_table))
+csv_writer.writerow(["Kategória/Kluby"] + list(clubs))
+for clas in classes:
+    points = [class_points.get(clas, 0) for class_points in point_maps]
+    csv_writer.writerow([clas] + points)
+
+total_points = [str(sum(class_points.values())) for class_points in point_maps]
+csv_writer.writerow(["Spolu"] + total_points)
